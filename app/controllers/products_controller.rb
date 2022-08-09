0fa19fb74bbbd1684f67938a239267d1a563a45c
Zihan Wang <zihan9701@gmail.com>
2022-08-09 16:18:53 -0700

    -[feat] add new_page products/search, not functioning

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -59,6 +59,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def search
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
   get 'home/index'
+  get 'products/search'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
